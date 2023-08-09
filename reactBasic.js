7466f720b4c83ef58e2cbc0275e3646876f7316a
kunaldhongade <kunaldhongade1@gmail.com>
2023-08-09 11:10:30 +0530

    First Commit

---
+++ reactBasic.js
@@ -0,0 +1,30 @@
+/*
+
+why React
+    1. single page application
+        js heavy to load but interactive
+
+    2. Part of MultiPageApp (MPA)
+
+What is React
+    Ui library of components
+
+
+Component based approach
+
+Components
+    1. easy structure
+    2. re-User
+
+                    App
+
+        Search      Sidebar      Content
+                                    |
+                            Cover   Intro   Videos
+                                                |
+                                        playlist    Playlist    Playlist
+                                                                     |   
+                                                            Video   Video   Video
+
+
+*/
