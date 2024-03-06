c5ded74661287205faa6728c5597387345b96e4f
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-06 18:19:00 +0530

    add: misc

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const Clock = () => {
     let t = new Date().toLocaleTimeString()
@@ -20,5 +20,3 @@ const Clock = () => {
 }
 
 export default Clock
-
-// passing state through component
